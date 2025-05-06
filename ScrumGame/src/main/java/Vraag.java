bf9b298497c79e35df53e7577dee08102ff19ca2
Daan Krol <daankrol06@gmail.com>
2025-05-06 12:12:38 +0200

    vraag fix

---
+++ ScrumGame/src/main/java/Vraag.java
@@ -0,0 +1,4 @@
+public abstract class Vraag {
+    String vraag;
+    String antwoord;
+}
--- ScrumGame/src/main/java/Vragen.java
+++
@@ -1,6 +0,0 @@
-public abstract class Vragen {
-    private String Vraag;
-    private String Antwoord;
-
-
-}
