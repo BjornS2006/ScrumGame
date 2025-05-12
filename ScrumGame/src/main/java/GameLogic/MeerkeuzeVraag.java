e67b5200450c3e50112e5059a2661279bbd5dd2c
Hugo0000000 <24087823@student.hhs.nl>
2025-05-12 12:37:38 +0200

    project dingen

--- ScrumGame/src/main/java/GameLogic/MeerkeuzeVraag.java
+++ ScrumGame/src/main/java/GameLogic/MeerkeuzeVraag.java
@@ -1,20 +1,49 @@
 package GameLogic;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MeerkeuzeVraag implements VraagType {
     private String vraagNaam;
+    private int antwoord;
+    Scanner scanner = new Scanner(System.in);
+    private ArrayList <String> antwoordMogelijkheden;
+    private Monster monster;
 
-    public StringVraagNaam () {
+    public MeerkeuzeVraag(Monster monster, ArrayList<String> antwoordMogelijkheden, int antwoord, String vraagNaam) {
+        this.monster = monster;
+        this.antwoordMogelijkheden = antwoordMogelijkheden;
+        this.antwoord = antwoord;
+        this.vraagNaam = vraagNaam;
+    }
 
+    @Override
+    public String getVraagNnaam() {
+        return vraagNaam;
     }
 
 
     @Override
     public void stelVraag() {
-        System.out.println("Dit is de " + );
+        System.out.println("Dit is:  " + vraagNaam + "?");
+        for (int i = 0; i < antwoordMogelijkheden.size (); i++) {
+            System.out.println(antwoordMogelijkheden.get(i));
+        }
+        String input = scanner.nextLine();
+        checkGoedAntwoord(input);
     }
 
     @Override
-    public void checkGoedAntwoord() {
+    public void checkGoedAntwoord(String input) {
+        String goedAntwoord = antwoordMogelijkheden.get(antwoord);
+        String[] delen = goedAntwoord.split(":");
+        String goedeLetter = delen[0];
 
+        if (input.toUpperCase().equals(goedeLetter)){
+            System.out.println("goed");
+        }else {
+            monster.monsterVerschijnt();
+        }
     }
 }
