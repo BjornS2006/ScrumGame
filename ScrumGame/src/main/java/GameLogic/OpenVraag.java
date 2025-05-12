b6f3db341310ddf9d69c63cccce588cadcab7de6
Daan Krol <daankrol06@gmail.com>
2025-05-12 12:36:23 +0200

    Openvragem

--- ScrumGame/src/main/java/GameLogic/OpenVraag.java
+++ ScrumGame/src/main/java/GameLogic/OpenVraag.java
@@ -1,7 +1,18 @@
 package GameLogic;
 
+import java.util.Scanner;
+
 public class OpenVraag implements VraagType {
     private String vraagNaam;
+    private String antwoord;
+    Scanner scanner = new Scanner(System.in);
+    private Monster monster;
+
+    public OpenVraag(GameLogic.Monster monster, String antwoord, String vraagNaam) {
+        this.monster = monster;
+        this.antwoord = antwoord;
+        this.vraagNaam = vraagNaam;
+    }
 
     @Override
     public String getVraagNaam() {
@@ -9,17 +20,18 @@ public class OpenVraag implements VraagType {
     }
 
     @Override
-    public void setVraagNaam(vraagNaam) {
-        this.vraagNaam = vr;
-    }
-
-    @Override
     public void stelVraag() {
-        System.out.println(``````);
+        System.out.println(vraagNaam);
+        String input = scanner.nextLine();
+        checkGoedAntwoord(input);
     }
 
     @Override
-    public void checkGoedAntwoord() {
-
+    public void checkGoedAntwoord(String input) {
+        if (input.equals(antwoord)) {
+            System.out.println("Vraag goed!");
+        } else {
+            monster.monsterVerschijnt();
+        }
     }
 }
