b4612271cbc49bb0c24110227ec7390a8c827980
Jorn <24112267@hhs.student.nl>
2025-06-06 13:40:42 +0200

    new save system back in the main

--- ScrumGame/src/main/java/utils/SaveSystem.java
+++ ScrumGame/src/main/java/utils/SaveSystem.java
@@ -2,8 +2,13 @@ package utils;
 
 import gamelogic.Speler;
 import gamelogic.Status;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import usableitems.IUsebleItem;
+import usableitems.Zwaart;
+import usableitems.Komkommer;
+import usableitems.KamerInforBoek;
+import joker.KeyJoker;
+import joker.HintJoker;
 
 import java.io.FileWriter;
 import java.io.FileReader;
@@ -12,7 +17,28 @@ import java.io.File;
 
 public class SaveSystem implements SaveSystemInterface {
     private static final String FILE_PATH = "Save.json";
-    private static final Gson gson = new GsonBuilder().create();
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(IUsebleItem.class, new JsonDeserializer<IUsebleItem>() {
+                @Override
+                public IUsebleItem deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                    JsonObject obj = json.getAsJsonObject();
+                    String type = obj.get("type").getAsString();
+                    return switch (type) {
+                        case "Zwaart" -> context.deserialize(json, Zwaart.class);
+                        case "Komkommer" -> context.deserialize(json, Komkommer.class);
+                        case "KamerInforBoek" -> context.deserialize(json, KamerInforBoek.class);
+                        case "KeyJoker" -> context.deserialize(json, KeyJoker.class);
+                        case "HintJoker" -> context.deserialize(json, HintJoker.class);
+                        default -> throw new JsonParseException("Unknown item type: " + type);
+                    };
+                }
+            })
+            .registerTypeAdapter(IUsebleItem.class, (JsonSerializer<IUsebleItem>) (src, typeOfSrc, context) -> {
+                JsonObject obj = (JsonObject) context.serialize(src, src.getClass());
+                obj.addProperty("type", src.getClass().getSimpleName());
+                return obj;
+            })
+            .create();
 
     @Override
     public void saveGame(Speler speler) {
@@ -45,20 +71,15 @@ public class SaveSystem implements SaveSystemInterface {
     @Override
     public void reset() {
         try (FileWriter writer = new FileWriter(FILE_PATH)) {
-            writer.write(""); // Clear the file content
+            writer.write("");
         } catch (IOException e) {
             System.err.println("Error resetting save file: " + e.getMessage());
         }
-        // Ensure a default Speler is created after reset
         saveGame(createDefaultSpeler());
     }
-
-
 
     private Speler createDefaultSpeler() {
         Status defaultStatus = new Status(0, "Start", this);
         return new Speler(defaultStatus);
     }
 }
-
-
